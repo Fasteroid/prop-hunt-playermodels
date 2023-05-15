361ab949ef4a87800369151cc6b7caef61e18811
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2023-05-15 17:15:50 -0400

    2024_flash_playermodel_npcs_951249553

---
+++ package.js
@@ -0,0 +1,37 @@
+const subprocess = require('child_process');
+const fs = require('fs');
+
+const SOURCE = "C:\\Users\\Faste\\Desktop\\misc_stuff\\addons"
+
+const getFileReg = /\/?([^\/]*?\..[^\/]*)$/gm
+/**
+* @param {String} path 
+* @returns is it a directory?
+*/
+function isDir(path){
+    const result = path.match(getFileReg);
+    return !result;
+}
+
+function syncExec(cmd, callback){
+    return new Promise( (resolve, reject) => {
+        subprocess.exec(cmd, (a,b,c) => {
+            try{ callback(a,b,c) }
+            catch( e ){ reject(e) }
+            resolve("exited")
+        });
+    })
+}
+
+const files = fs.readdirSync(SOURCE)
+
+async function main(){
+    for( let addon of files ){
+        let path = SOURCE + "\\" + addon
+        if( !isDir(path) ){ continue; }
+        console.log(path)
+        await syncExec(`xcopy ${path} . /e /d /y /h /r /c /q`, console.log)
+        await syncExec(`git add . & git commit -m${addon}`, console.log)
+    }
+} 
+main()
